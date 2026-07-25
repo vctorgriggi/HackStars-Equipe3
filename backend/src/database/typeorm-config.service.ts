7b6c0ebd266929f37afd0f6a24da8ccd7b33621c
Wisley Rodrigues <wrdp.roo10@hotmail.com>
2026-07-25 14:38:18 -0400

    feat: change ssl insecurity true

--- backend/src/database/typeorm-config.service.ts
+++ backend/src/database/typeorm-config.service.ts
@@ -30,28 +30,7 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
 
         subscribersDir: 'subscriber',
       },
-      extra: {
-        // based on https://node-postgres.com/apis/pool
-        // max connection pool size
-        max: this.configService.get('database.maxConnections', { infer: true }),
-        ssl: this.configService.get('database.sslEnabled', { infer: true })
-          ? {
-              rejectUnauthorized: this.configService.get(
-                'database.rejectUnauthorized',
-                { infer: true },
-              ),
-              ca:
-                this.configService.get('database.ca', { infer: true }) ??
-                undefined,
-              key:
-                this.configService.get('database.key', { infer: true }) ??
-                undefined,
-              cert:
-                this.configService.get('database.cert', { infer: true }) ??
-                undefined,
-            }
-          : undefined,
-      },
+      ssl: { rejectUnauthorized: false },
     } as TypeOrmModuleOptions;
   }
 }
